bdc923a36231867dbc0f3fc8e921dc8b61d9e623
Veronica Hall <86692664+ver0nicahall@users.noreply.github.com>
2022-04-25 03:58:25 -0500

    further study: addTradingCard feature

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -59,10 +59,45 @@ function TradingCard(props) {
   );
 }
 
+function AddTradingCard(props) {
+  const [name, setName] = React.useState("");
+  const [skill, setSkill] = React.useState("");
+  function addNewCard() {
+    // TO BE IMPLEMENTED
+    alert('trying to add new card');
+  }
+  return (
+    <React.Fragment>
+      <h2>Add New Trading Card</h2>
+      <label htmlFor="nameInput">Name</label>
+      <input
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+        id="nameInput"
+        style={{ marginLeft: "5px" }}
+      ></input>
+      <label
+        htmlFor="skillInput"
+        style={{ marginLeft: "10px", marginRight: "5px" }}
+      >
+        Skill
+      </label>
+      <input
+        value={skill}
+        onChange={(event) => setSkill(event.target.value)}
+        id="skillInput"
+      ></input>
+      <button style={{ marginLeft: "10px" }} onClick={addNewCard}>
+        Add
+      </button>
+    </React.Fragment>
+  );
+}
+
 function TradingCardContainer() {
 
   const [cards, setCards] = React.useState([]);
-
+  
   React.useEffect(() => {
     fetch('/cards.json')
     .then((response) => response.json())
@@ -82,7 +117,15 @@ function TradingCardContainer() {
     );
   }
 
-  return <div className="grid">{tradingCards}</div>;
+  return (
+    <React.Fragment>
+      <AddTradingCard />
+      <h2>Trading Cards</h2>
+      <div className="grid">{tradingCards}</div>
+    </React.Fragment>
+  );
+
 }
+
 
 ReactDOM.render(<TradingCardContainer />, document.getElementById('container'));
